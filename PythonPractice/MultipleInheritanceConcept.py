6ffe074b55a80e3fa1ca517eed6d8520c1fd4ad1
Sandeep <is.sandeep7@gmail.com>
2019-08-07 00:02:17 +0530

    Added Multiple Inheritance Concept

---
+++ PythonPractice/MultipleInheritanceConcept.py
@@ -0,0 +1,34 @@
+class Base1(object):
+
+    def __init__(self):
+        self.str1 = "Sandeep"
+        print("Base1")
+
+class Base2(object):
+
+    def __init__(self):
+        self.str2 = "Tom"
+        print("Base2")
+
+
+class Child(Base1, Base2):  #Achieved multiple inheritance
+    def __init__(self):
+        Base1.__init__(self)
+        Base2.__init__(self)
+
+        print("Child")
+
+    def printStrings(self):
+        print(self.str1)
+        print(self.str2)
+
+ob = Child()    #this object will call the constructor
+ob.printStrings()
+'''
+output:
+Base1
+Base2
+Child
+Sandeep
+Tom
+'''
